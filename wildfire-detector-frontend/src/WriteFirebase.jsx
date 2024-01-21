4e435b0a46410b392e071353c025f9dad88dc435
markestiller <markalestiller@gmail.com>
2024-01-21 10:40:46 -0500

    implemented code structure to integrate esp server to web and web to firebase

--- wildfire-detector-frontend/src/WriteFirebase.jsx
+++ wildfire-detector-frontend/src/WriteFirebase.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { getDatabase, ref, set } from "firebase/database";
 
-function WriteFirebase() {
+function WriteFirebase(locationId, { temperature, humidity }) {
+  const db = getDatabase();
+  set(ref(db, "locations/" + locationId), {
+    temperature: temperature,
+    humidity: humidity,
+  })
+    .then(() => {
+      console.log("Data saved successfully!");
+    })
+    .catch((err) => console.log(err));
   return <div>WriteFirebase</div>;
 }
 
